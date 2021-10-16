b0c8702ec421f9ee03bbe90a8048f07e4410d716
jartigag <jartigag@pm.me>
2021-10-16 13:24:48 +0200

    Step 1
    Co-authored-by: yai1394 <yai1394@users.noreply.github.com>

--- test/string_calculator.test.ts
+++ test/string_calculator.test.ts
@@ -2,4 +2,16 @@ import StringCalculator from '../src/string_calculator'
 
 describe('Calculator', () => {
     const calculator = new StringCalculator()
+
+    it('empty string returns 0', () => {
+        expect(calculator.Add('')).toEqual(0)
+    })
+
+    it('"1" returns 1', () => {
+        expect(calculator.Add("1")).toEqual(1)
+    })
+
+    it('"1,2" returns 3', () => {
+        expect(calculator.Add("1,2")).toEqual(3)
+    })
 })
